5bfd3661637cf318a52291702304ed40e9f995a1
Nagasudhir Pulla <Nagasudhir@wrldc.local>
2019-12-02 18:04:09 +0530

    ict data push working

--- src/index.py
+++ src/index.py
@@ -2,8 +2,8 @@ import datetime as dt
 from fileHandler import FileHandler
 
 # input variables
-fileId = 'volt'
+fileId = 'ict'
 startDt = dt.datetime.now() - dt.timedelta(days=4)
-endDt = dt.datetime.now() - dt.timedelta(days=3)
+endDt = dt.datetime.now() - dt.timedelta(days=1)
 fileHandler = FileHandler()
 fileHandler.pushFileDataToDb(fileId, startDt, endDt)
