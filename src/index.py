72d04368f5f76f1053657aabd22f2f73ea839b2f
Nagasudhir Pulla <Nagasudhir@wrldc.local>
2019-12-02 11:40:55 +0530

    started poc

--- src/index.py
+++ src/index.py
@@ -1,2 +1,22 @@
 import pandas as pd
-print("Hello world!")
+import datetime as dt
+import os
+
+targetDt = dt.datetime.now() - dt.timedelta(days=1)
+
+fileMasterDf = pd.read_excel('master_data/scada_files_info_master.xlsx',
+                             sheet_name='files')
+
+fileInfoRow = fileMasterDf.iloc[0]
+folderPath = fileInfoRow.folderPath
+fileNamePrefix = fileInfoRow.prefix
+fileDateFormat = fileInfoRow.dateFormat
+fileType = fileInfoRow.fileType
+fileNameSuffix = fileInfoRow.suffix
+headerSkip = fileInfoRow.headerSkip
+footerSkip = fileInfoRow.footerSkip
+derivedFileName = fileNamePrefix + dt.datetime.strftime(targetDt, fileDateFormat) + fileNameSuffix
+fullfilePath = os.path.join(folderPath, derivedFileName)
+voltDf = None
+if os.path.exists(fullfilePath):
+    voltDf = pd.read_csv(fullfilePath, header=headerSkip, skipfooter=footerSkip)
