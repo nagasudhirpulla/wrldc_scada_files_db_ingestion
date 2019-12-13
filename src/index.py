00878980bffa416bf95e5b8e66c161d47005cd23
Nagasudhir Pulla <Nagasudhir@wrldc.local>
2019-12-13 18:02:02 +0530

    minor changes

--- src/index.py
+++ src/index.py
@@ -4,7 +4,8 @@ from fileHandler import FileHandler
 # input variables
 fileId = 'ict'
 # startDt = dt.datetime.now() - dt.timedelta(days=4)
-startDt = dt.datetime(2019, 2, 24)
+startDt = dt.datetime(2019, 1, 1)
+# endDt = dt.datetime(2019, 1, 10)
 endDt = dt.datetime.now() - dt.timedelta(days=1)
 fileHandler = FileHandler()
 fileHandler.pushFileDataToDb(fileId, startDt, endDt)
