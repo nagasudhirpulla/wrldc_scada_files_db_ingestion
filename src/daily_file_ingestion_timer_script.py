033d288dcc1a2ac357bba784437a0f1b70e58121
Nagasudhir Pulla <Nagasudhir@wrldc.local>
2019-12-14 23:27:39 +0530

    daily timer scipt added

--- src/daily_file_ingestion_timer_script.py
+++ src/daily_file_ingestion_timer_script.py
@@ -2,10 +2,12 @@ import datetime as dt
 from fileHandler import FileHandler
 
 # input variables
-fileId = 'volt'
+fileIds = ['volt', 'ict']
 # startDt = dt.datetime.now() - dt.timedelta(days=4)
-startDt = dt.datetime(2019, 1, 1)
+startDt = dt.datetime.now() - dt.timedelta(days=1)
 # endDt = dt.datetime(2019, 1, 10)
 endDt = dt.datetime.now() - dt.timedelta(days=1)
 fileHandler = FileHandler()
-fileHandler.pushFileDataToDb(fileId, startDt, endDt)
+
+for fileId in fileIds:
+    fileHandler.pushFileDataToDb(fileId, startDt, endDt)
