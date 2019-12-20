d7bf775576f700973a233f0d3f87ae4b438a70f6
Nagasudhir Pulla <Nagasudhir@wrldc.local>
2019-12-20 17:41:58 +0530

    demand fetch

--- src/index.py
+++ src/index.py
@@ -2,8 +2,8 @@ import datetime as dt
 from fileHandler import FileHandler
 
 # input variables
-fileIds = ['gen_sch_act', 'sch_act_ui', 'state_gen']
-startDt = dt.datetime(2019, 6, 1)
+fileIds = ['demand']
+startDt = dt.datetime(2018, 1, 1)
 # startDt = dt.datetime.now() - dt.timedelta(days=1)
 endDt = dt.datetime(2019, 12, 19)
 # endDt = dt.datetime.now() - dt.timedelta(days=1)
