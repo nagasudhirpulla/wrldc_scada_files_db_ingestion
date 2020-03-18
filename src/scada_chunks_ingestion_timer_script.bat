6f4b3adb0b0b2dc57048265eb81159ac34ce649a
Nagasudhir Pulla <Nagasudhir@wrldc.local>
2020-03-18 11:06:59 +0530

    folder change

--- src/scada_chunks_ingestion_timer_script.bat
+++ src/scada_chunks_ingestion_timer_script.bat
@@ -1,3 +1,3 @@
 CD /d %~dp0
 call C:\ProgramData\Anaconda3\Scripts\activate base
-call C:\ProgramData\Anaconda3\python.exe scadaChunksIngestionTimerScript.py --chunksFolder E:\Nagasudhir\EdnaDumps
+call C:\ProgramData\Anaconda3\python.exe scadaChunksIngestionTimerScript.py --chunksFolder \\10.2.100.51\sitenew\EdnaChunks
