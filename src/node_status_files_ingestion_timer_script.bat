81eaaee87d932a24223e9e5202cfedba41d4d052
nagasudhirpulla <nagasudhirpulla@gmail.com>
2020-06-11 10:59:09 +0530

    batch file for node status files

---
+++ src/node_status_files_ingestion_timer_script.bat
@@ -0,0 +1,3 @@
+CD /d %~dp0
+call C:\ProgramData\Anaconda3\Scripts\activate base
+call C:\ProgramData\Anaconda3\python.exe scadaChunksIngestionTimerScript.py --filesFolder \\10.2.100.51\sitenew\NodeStatusFiles
