97ab42c82c622adc36bdbc928f59400610d2cd05
nagasudhirpulla <nagasudhirpulla@gmail.com>
2020-06-11 09:46:59 +0530

    dem freq batch file

---
+++ src/scada_dem_freq_chunks_ingestion_timer_script.bat
@@ -0,0 +1,3 @@
+CD /d %~dp0
+call C:\ProgramData\Anaconda3\Scripts\activate base
+call C:\ProgramData\Anaconda3\python.exe scadaChunksIngestionTimerScript.py --chunksFolder \\10.2.100.51\sitenew\EdnaDemFreqChunks
