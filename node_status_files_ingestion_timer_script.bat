081004e3c80ddcfa3c3bf3c1cfbe242fad1f7ae4
Nagasudhir Pulla <Nagasudhir@wrldc.local>
2021-03-16 11:07:42 +0530

    changes

--- node_status_files_ingestion_timer_script.bat
+++ node_status_files_ingestion_timer_script.bat
@@ -1,5 +1,5 @@
 REM CD /d %~dp0
 REM call C:\ProgramData\Anaconda3\Scripts\activate base
-REM call C:\ProgramData\Anaconda3\python.exe nodeStatusFilesIngestionTimerScript.py --filesFolder \\10.2.100.51\sitenew\NodeStatusFiles
+REM call C:\ProgramData\Anaconda3\python.exe nodeStatusFilesIngestionTimerScript.py --filesFolder \\10.2.100.55\e$\scada_reports\FEChunks
 call project_env\Scripts\activate.bat
-call python nodeStatusFilesIngestionTimerScript.py --filesFolder \\10.2.100.51\sitenew\NodeStatusFiles
+call python nodeStatusFilesIngestionTimerScript.py --filesFolder \\10.2.100.55\e$\scada_reports\FEChunks
--- scada_dem_freq_chunks_ingestion_timer_script.bat
+++ scada_dem_freq_chunks_ingestion_timer_script.bat
@@ -1,5 +1,5 @@
 REM CD /d %~dp0
 REM call C:\ProgramData\Anaconda3\Scripts\activate base
-REM call C:\ProgramData\Anaconda3\python.exe scadaChunksIngestionTimerScript.py --chunksFolder \\10.2.100.51\sitenew\EdnaDemFreqChunks
+REM call C:\ProgramData\Anaconda3\python.exe scadaChunksIngestionTimerScript.py --chunksFolder \\10.2.100.55\e$\scada_reports\EdnaDemFreqChunks
 call project_env\Scripts\activate.bat
-python scadaChunksIngestionTimerScript.py --chunksFolder \\10.2.100.51\sitenew\EdnaDemFreqChunks
+python scadaChunksIngestionTimerScript.py --chunksFolder \\10.2.100.55\e$\scada_reports\EdnaDemFreqChunks
